f0ab6364b948564ca50d7b58d5c4eba243d53fb5
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-11-08 14:40:03 -0700

    started on resolvers and typedefs. Need to add query files still

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,15 @@
+const { User } = require('../models');
+const { signToken, authMiddleware } = require('../utils/auth')
 
 const resolvers = {
-    
+    Query: {
+        getAllUsers: async () => {
+            return User.find();
+        },
+        getSingleUser: async (parent, { userId }) => {
+            return User.findOne({ _id: userId })
+        }
+    }
 }
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,21 @@
 const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+    }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        getAllUsers: [User]!
+        getSingleUser(userId: ID!): User
+    }
 `;
 
 module.exports = typeDefs;
