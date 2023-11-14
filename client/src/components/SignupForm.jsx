23ecb1b0374acf146edf7cb3560dee72b60cff1b
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-11-13 18:31:49 -0700

    working on mutation functionality

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
+import { createUser } from '../'
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
